54adb3cfb059c80f90ca225ca7bca944098f5793
Shau Yin Luk <ivan.luk@icloud.com>
2020-06-10 16:45:36 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
-export function managePresents(state, action){
+export function managePresents(state = {numberOfPresents: 0}, action){
+  switch (action.type) { 
+    case "INCREASE":
+      let newCount = {...state, numberOfPresents: state.numberOfPresents + 1}
+      return newCount
+        default:
+      return state
+    }
 }
